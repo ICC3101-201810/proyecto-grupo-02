91329b032012ce7e447c2add5239208152943b34
Panchoapc <30845908+Panchoapc@users.noreply.github.com>
2018-04-17 11:55:56 -0300

    Creada las interfaces de verificacion y la clase MyNotes

---
+++ MyNotes/My Notes/My Notes/MyNotes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Notes
+{
+    class MyNotes
+    {//DOS LISTAS porque no podemos hacer lista de Usuarios y meterle porfesores y alumnos.
+        List<Alumno> ListaAlumnos;
+        List<Profesor> ListaProfesores;
+
+        public MyNotes()
+        {
+            ListaAlumnos = new List<Alumno>();
+            ListaProfesores = new List<Profesor>();
+        }
+        public void AgregarAlumnos(Alumno alumno)
+        {
+            ListaAlumnos.Add(alumno);
+        }
+        public void AgregarProfesor(Profesor profesor)
+        {
+            ListaProfesores.Add(profesor);
+        }
+    }
+}

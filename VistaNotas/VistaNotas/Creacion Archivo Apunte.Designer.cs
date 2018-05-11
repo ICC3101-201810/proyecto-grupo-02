bd4058495f995c1b9afcf07a785f7d6bcd1e629e
Panchoapc <30845908+Panchoapc@users.noreply.github.com>
2018-05-11 12:10:14 -0300

    Nuevo form Crear apuntes

---
+++ VistaNotas/VistaNotas/Creacion Archivo Apunte.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace VistaNotas
+{
+    partial class Creacion_Archivo_Apunte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CBTipo = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TBCapitulo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TPFecha = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tipo";
+            // 
+            // CBTipo
+            // 
+            this.CBTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CBTipo.FormattingEnabled = true;
+            this.CBTipo.Location = new System.Drawing.Point(376, 126);
+            this.CBTipo.Name = "CBTipo";
+            this.CBTipo.Size = new System.Drawing.Size(259, 33);
+            this.CBTipo.TabIndex = 1;
+            this.CBTipo.SelectedIndexChanged += new System.EventHandler(this.CBTipo_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // TBNombre
+            // 
+            this.TBNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBNombre.Location = new System.Drawing.Point(376, 69);
+            this.TBNombre.Name = "TBNombre";
+            this.TBNombre.Size = new System.Drawing.Size(258, 30);
+            this.TBNombre.TabIndex = 3;
+            this.TBNombre.Text = "SinNombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(257, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Creacion de archivo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(121, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Capitulo/Tema";
+            // 
+            // TBCapitulo
+            // 
+            this.TBCapitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBCapitulo.Location = new System.Drawing.Point(376, 194);
+            this.TBCapitulo.Name = "TBCapitulo";
+            this.TBCapitulo.Size = new System.Drawing.Size(258, 30);
+            this.TBCapitulo.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(121, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Fecha";
+            // 
+            // TPFecha
+            // 
+            this.TPFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TPFecha.Location = new System.Drawing.Point(376, 273);
+            this.TPFecha.Name = "TPFecha";
+            this.TPFecha.Size = new System.Drawing.Size(256, 30);
+            this.TPFecha.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(183, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Cancelar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(526, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 46);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Crear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Creacion_Archivo_Apunte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 479);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TPFecha);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TBCapitulo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TBNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CBTipo);
+            this.Controls.Add(this.label1);
+            this.Name = "Creacion_Archivo_Apunte";
+            this.Text = "Creacion_Archivo_Apunte";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CBTipo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TBCapitulo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker TPFecha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
--- VistaNotas/VistaNotas/Seleccionar Ramo Para Crear Apunte.Designer.cs
+++ VistaNotas/VistaNotas/Seleccionar Ramo Para Crear Apunte.Designer.cs
@@ -38,26 +38,29 @@
             // comboBox_Seleccion_Ramo
             // 
             this.comboBox_Seleccion_Ramo.FormattingEnabled = true;
-            this.comboBox_Seleccion_Ramo.Location = new System.Drawing.Point(402, 81);
+            this.comboBox_Seleccion_Ramo.Location = new System.Drawing.Point(536, 100);
+            this.comboBox_Seleccion_Ramo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox_Seleccion_Ramo.Name = "comboBox_Seleccion_Ramo";
-            this.comboBox_Seleccion_Ramo.Size = new System.Drawing.Size(350, 21);
+            this.comboBox_Seleccion_Ramo.Size = new System.Drawing.Size(465, 24);
             this.comboBox_Seleccion_Ramo.TabIndex = 0;
             // 
             // LabelSeleccioneRamo
             // 
             this.LabelSeleccioneRamo.AutoSize = true;
             this.LabelSeleccioneRamo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelSeleccioneRamo.Location = new System.Drawing.Point(45, 77);
+            this.LabelSeleccioneRamo.Location = new System.Drawing.Point(60, 95);
+            this.LabelSeleccioneRamo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LabelSeleccioneRamo.Name = "LabelSeleccioneRamo";
-            this.LabelSeleccioneRamo.Size = new System.Drawing.Size(175, 24);
+            this.LabelSeleccioneRamo.Size = new System.Drawing.Size(219, 29);
             this.LabelSeleccioneRamo.TabIndex = 1;
             this.LabelSeleccioneRamo.Text = "Seleccione Ramo";
             // 
             // BotonAceptar
             // 
-            this.BotonAceptar.Location = new System.Drawing.Point(589, 195);
+            this.BotonAceptar.Location = new System.Drawing.Point(785, 240);
+            this.BotonAceptar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BotonAceptar.Name = "BotonAceptar";
-            this.BotonAceptar.Size = new System.Drawing.Size(163, 47);
+            this.BotonAceptar.Size = new System.Drawing.Size(217, 58);
             this.BotonAceptar.TabIndex = 2;
             this.BotonAceptar.Text = "Aceptar";
             this.BotonAceptar.UseVisualStyleBackColor = true;
@@ -65,31 +68,35 @@
             // 
             // Nombre_Nuevo_Apunte
             // 
-            this.Nombre_Nuevo_Apunte.Location = new System.Drawing.Point(406, 135);
+            this.Nombre_Nuevo_Apunte.Location = new System.Drawing.Point(541, 166);
+            this.Nombre_Nuevo_Apunte.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Nombre_Nuevo_Apunte.Name = "Nombre_Nuevo_Apunte";
-            this.Nombre_Nuevo_Apunte.Size = new System.Drawing.Size(345, 20);
+            this.Nombre_Nuevo_Apunte.Size = new System.Drawing.Size(459, 22);
             this.Nombre_Nuevo_Apunte.TabIndex = 3;
+            this.Nombre_Nuevo_Apunte.TextChanged += new System.EventHandler(this.Nombre_Nuevo_Apunte_TextChanged);
             // 
             // Label_Escriba_Nombre
             // 
             this.Label_Escriba_Nombre.AutoSize = true;
             this.Label_Escriba_Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label_Escriba_Nombre.Location = new System.Drawing.Point(44, 129);
+            this.Label_Escriba_Nombre.Location = new System.Drawing.Point(59, 159);
+            this.Label_Escriba_Nombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Label_Escriba_Nombre.Name = "Label_Escriba_Nombre";
-            this.Label_Escriba_Nombre.Size = new System.Drawing.Size(225, 24);
+            this.Label_Escriba_Nombre.Size = new System.Drawing.Size(277, 29);
             this.Label_Escriba_Nombre.TabIndex = 4;
             this.Label_Escriba_Nombre.Text = "Nombre Nuevo Apunte";
             // 
             // Seleccionar_Ramo_Para_Crear_Apunte
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.Label_Escriba_Nombre);
             this.Controls.Add(this.Nombre_Nuevo_Apunte);
             this.Controls.Add(this.BotonAceptar);
             this.Controls.Add(this.LabelSeleccioneRamo);
             this.Controls.Add(this.comboBox_Seleccion_Ramo);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Seleccionar_Ramo_Para_Crear_Apunte";
             this.Text = "Seleccionar_Ramo_Para_Crear_Apunte";
             this.ResumeLayout(false);
